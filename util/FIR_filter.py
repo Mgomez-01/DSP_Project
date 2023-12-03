5e3cbfa9aa8e1ab5df8e3832a30b82859ac5ded5
Miguel <u1318856@eng.utah.edu>
2023-12-03 01:56:38 -0700

    fixed issues with printing and fixed the frequency to no longer use normalized vals

--- util/FIR_filter.py
+++ util/FIR_filter.py
@@ -7,26 +7,24 @@ import matplotlib.pyplot as plt
 class FIRFilter:
     def __init__(self, N=10000, fmin=3, fmax=7, padding_factor=9, fs=8000):
         self.N = N
-        self.fmin = fmin
-        self.fmax = fmax
         self.padding_factor = padding_factor
         self.fs = fs  # Sampling rate
-        
         self.H = zeros(N)
         self.w = zeros(N)
         self.pos = np.arange(N)
+        self.fmin = fmin*self.N/self.fs
+        self.fmax = fmax*self.N/self.fs
+        
         self.h = None
         self.h_pad = None
         self.H_pad = None
         self.w_pad = None
         self.h_ham = None
         self.H_ham_pad = None
-
+        
         self.create_filter()
         self.apply_padding()
         self.apply_hamming_window()
-        self.buffer_length = 1024
-        self.buffer = np.zeros(self.buffer_length + len(self.h_ham) - 1)
 
 
     def create_filter(self):
@@ -44,31 +42,11 @@ class FIRFilter:
 
     def apply_hamming_window(self):
         self.h_ham = self.h * 0.5 * (1 + np.cos(2 * np.pi * (self.pos - self.N / 2) / self.N))
-    # =============================================================================
-    #         Below is the hamming function from the assignment, It doesn't quite work so we're using the one above,
-    #         The line above also works for a wide bandwidth passband instead of at a single frequency point "wc"
-    # =============================================================================
-        # self.h_ham = self.h*(0.54-0.46*np.cos(2*np.pi*self.pos/(self.N-1)))*np.cos((self.fmin+self.fmax)/2*(self.pos-(self.N-1)/2))
         self.h_ham_pad = append(self.h_ham, zeros(self.padding_factor * self.N))
         self.H_ham_pad = fftshift(fft(self.h_ham_pad)) / self.N
 
     def process(self, input_data):
-        # Append input data to buffer
-        self.buffer_length = len(input_data)
-        self.buffer = np.zeros(self.buffer_length + len(self.h_ham) - 1)
-        self.buffer[:-len(input_data)] = self.buffer[len(input_data):]
-        self.buffer[-len(input_data):] = input_data
-
-        # Perform FFT-based convolution
-        input_fft = np.fft.fft(self.buffer)
-        filter_fft = np.fft.fft(self.h_ham, n=len(self.buffer))
-        result_fft = input_fft * filter_fft
-
-        # Inverse FFT to get the time domain signal
-        result = np.fft.ifft(result_fft)
-
-        # Return the relevant part of the result
-        return result[-len(input_data):].real  # Returning only the real part
+        return np.convolve(input_data, self.h_ham)/self.N
 
     def plot_filter(self, fig, row, col, pos):
         # Frequency Response Plot
@@ -76,10 +54,42 @@ class FIRFilter:
         # ax1.scatter(self.w, self.H.real, c='b', s=150)
         # ax1.plot(self.w_pad, abs(self.H_pad), 'r')
         ax1.plot(self.w_pad, abs(self.H_ham_pad), 'black')
-        ax1.set_xlim(0, .5)
+        #ax1.set_xlim(0, .5)
         ax1.set_title(f'Frequency Response of Octave Filter {pos}', fontsize=5, fontweight='bold')
         if pos == row-1:            
             ax1.legend(['Hamming'], prop={'size': 5})
             ax1.set_xlabel('Frequency (Ratio of Fs)', fontsize=5, fontweight='bold')
             ax1.set_ylabel('Magnitude', fontsize=5, fontweight='bold')
         ax1.grid(True)
+
+    def plot_filter1(self):
+        # MatPlotLib plotting
+        fig = plt.figure(figsize=(22, 16))
+
+        # Frequency Response Plot
+        ax1 = fig.add_subplot(211)
+        ax1.scatter(self.w, self.H.real, c='b', s=150)
+        ax1.plot(self.w_pad, abs(self.H_pad), 'r')
+        ax1.plot(self.w_pad, abs(self.H_ham_pad), 'black')
+        ax1.set_xlabel('Frequency (Hz)', fontsize=15, fontweight='bold')
+        ax1.set_ylabel('Magnitude', fontsize=15, fontweight='bold')
+        ax1.set_title('Frequency Response of FIR Filter', fontsize=15, fontweight='bold')
+        ax1.legend(['Ideal', 'Actual', 'Hamming'], prop={'size': 15})
+        ax1.tick_params(axis='both', labelsize=15)
+        ax1.grid(True)
+
+        # Time Domain Plot
+        ax2 = fig.add_subplot(212)
+        ax2.vlines(self.pos, 0, self.h.real, 'b')
+        # ax2.vlines(self.pos, 0, self.h.imag, 'r')
+        ax2.scatter(self.pos, self.h.real, c='b', s=150)
+        # ax2.scatter(self.pos, self.h.imag, c='r', s=150)
+        ax2.set_xlabel('Position', fontsize=15, fontweight='bold')
+        ax2.set_ylabel('Value (Unscaled)', fontsize=15, fontweight='bold')
+        ax2.set_title('Time Domain FIR Filter', fontsize=15, fontweight='bold')
+        # ax2.legend(['Real', 'Imag'], prop={'size': 15})
+        ax2.tick_params(axis='both', labelsize=15)
+        ax2.grid(True)
+
+        plt.show(block=False)
+
