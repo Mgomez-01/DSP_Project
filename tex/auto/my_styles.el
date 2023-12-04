5f5220e31764797dcd43e9a1c2af153de4941e1f
Miguel Gomez <u1318856@eng.utah.edu>
2023-12-04 16:02:52 -0700

    adding in tex files for the turnin stuff

---
+++ tex/auto/my_styles.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "my_styles"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "listings"
+    "color")
+   (LaTeX-add-listings-lstdefinestyles
+    "mystyle")
+   (LaTeX-add-xcolor-definecolors
+    "codegreen"
+    "codegray"
+    "codepurple"
+    "backcolour"))
+ :latex)
+
